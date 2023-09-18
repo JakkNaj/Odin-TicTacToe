1617800dd18ad520a2273518ab06f893c72fefdc
Jakub Najman <jakub@najman.cz>
2023-09-18 22:47:00 +0200

    game logic done

--- script.js
+++ script.js
@@ -40,6 +40,11 @@ const gameController = (() => {
             console.log("draw");
             gamePlaying = false;
         }
+        if (checkWinCombinations(index)) {
+            let playerSign = getPlayerSign();
+            console.log(`Player with sign: ${playerSign} wins!`);
+            gamePlaying = false;
+        }
         round++;
     };
 
@@ -49,6 +54,30 @@ const gameController = (() => {
 
     const getGamePlaying = () => {
         return gamePlaying;
+    }
+
+    const winConditions = [
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
+        [0, 4, 8],
+        [2, 4, 6],
+    ];
+
+    const checkWinCombinations = (index) => {
+        let winConsWithSameIdx = winConditions.filter( (c) => c.includes(index));
+        return winConsWithSameIdx.some( (combination) => {
+            let outcome = true;
+            combination.forEach((num) => {
+                if (gameboard.getField(num) !== getPlayerSign()){
+                    outcome = false;
+                }
+            })
+            return outcome;
+        })
     }
 
     return {
@@ -62,7 +91,6 @@ const displayController = (() => {
     gTiles.forEach((tile) => {
         tile.addEventListener("click", (e) => {
             if (!gameController.getGamePlaying() || e.target.textContent !== "") return;
-            console.log(e.target.getAttribute("idx"));
             gameController.playRound(parseInt(e.target.getAttribute("idx")));
             updateGameboard();
         })
